488c4c0c647edc03316899ff7c2cabbc87995b3b
Xolmo <a20220378@pucp.edu.pe>
2025-06-21 20:14:25 -0500

    ok

---
+++ webapp/LAB9_Tarea_20220378/src/test/java/com/example/lab9_tarea_20220378/Lab9Tarea20220378ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab9_tarea_20220378;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab9Tarea20220378ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
